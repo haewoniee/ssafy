6a29c75c4df935d76e26d43847451e6dddc24118
한혜원 <hhan9243@gmail.com>
2020-08-04 16:06:59 +0900

    swea 사칙연산 유효성검사

--- src/com/ssafy/swea/p1233_사칙연산유효성검사/Solution.java
+++ src/com/ssafy/swea/p1233_사칙연산유효성검사/Solution.java
@@ -3,53 +3,132 @@ package com.ssafy.swea.p1233_사칙연산유효성검사;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
 public class Solution {
 	
 	static int T = 10, N;
 	
-	static class Node {
-		Node left, right;
-		int nodeNum;
-		String data;
-		
-		public Node(int nodeNum, String data) {
-			this.left = null;
-			this.right = null;
-			this.nodeNum = nodeNum;
-			this.data = data;
-		}
-		
-		public void add(String data, int nodeNum)
-		{
-			//왼쪽 자식이 비었으면 왼쪽부터 넣기
-			if (left == null)
-			{
-				left = new Node(nodeNum, data);
-			}
-			else if (right == null)
-			{
-				right = new Node(nodeNum, data);
-			}
-			else
-			{
-				//왼쪽 자식의 자식들이 다 안찼으면 왼쪽에 넣기
-				if (left.left == null || left.right == null)
-				{
-					left.add(data, nodeNum);
-				}
-				else if (right.left == null || right.right == null)
-				{
-					right.add(data, nodeNum);
-				}
-				else
-				{
-					left.add(data, nodeNum);
-				}
-			}
-		}
-		
-	}
+//	static class Node {
+//		Node left, right;
+//		int nodeNum;
+//		String data;
+//		
+//		public Node(int nodeNum)
+//		{
+//			this.nodeNum = nodeNum;
+//			this.left = null;
+//			this.right = null;
+//		}
+//		
+//		public int size()
+//		{
+//			int s = 1;
+//			if (left != null)
+//			{
+//				s += left.size();
+//			}
+//			if (right != null)
+//			{
+//				s += right.size();
+//			}
+//			return s;
+//		}
+//		
+//		public boolean add(String data, int nodeNum)
+//		{
+//			boolean flag = false;
+//			if (nodeNum == this.nodeNum)
+//			{
+//				this.data = data;
+//				flag = true;
+//			}
+//			else if (this.left != null && nodeNum == this.left.nodeNum)
+//			{
+//				flag = left.add(data, nodeNum);
+//			}
+//			else if (this.right != null && nodeNum == this.right.nodeNum)
+//			{
+//				flag = right.add(data, nodeNum);
+//			}
+//			else
+//			{
+//				if (this.left != null)
+//				{
+//					flag = left.add(data, nodeNum);					
+//				}
+//				if (this.right != null && !flag)
+//				{
+//					flag = right.add(data, nodeNum);
+//				}
+//			}
+//			return flag;
+//		}
+//		public boolean add(String data, int nodeNum, int leftNum, int rightNum)
+//		{
+//			boolean flag = false;
+//			if (nodeNum == this.nodeNum)
+//			{
+//				this.data = data;
+//				this.left = new Node(leftNum);
+//				this.right = new Node(rightNum);
+//				flag = true;
+//			}
+//			else if (this.left != null && nodeNum == this.left.nodeNum)
+//			{
+//				flag = left.add(data, nodeNum, leftNum, rightNum);
+//			}
+//			else if (this.right != null && nodeNum == this.right.nodeNum)
+//			{
+//				flag = right.add(data, nodeNum, leftNum, rightNum);
+//			}
+//			else
+//			{
+//				if (this.left != null)
+//				{
+//					flag = left.add(data, nodeNum, leftNum, rightNum);					
+//				}
+//				if (this.right != null && !flag)
+//				{
+//					flag = right.add(data, nodeNum, leftNum, rightNum);
+//				}
+//			}
+//			return flag;
+//		}
+//		public boolean add(String data, int nodeNum, int leftNum)
+//		{
+//			boolean flag = false;
+//			if (nodeNum == this.nodeNum)
+//			{
+//				this.data = data;
+//				this.left = new Node(leftNum);
+//				flag = true;
+//			}
+//			else if (this.left != null && nodeNum == this.left.nodeNum)
+//			{
+//				flag = left.add(data, nodeNum, leftNum);
+//			}
+//			else if (this.right != null && nodeNum == this.right.nodeNum)
+//			{
+//				flag = right.add(data, nodeNum, leftNum);
+//			}
+//			else
+//			{
+//				if (this.left != null)
+//				{
+//					flag = left.add(data, nodeNum, leftNum);					
+//				}
+//				if (this.right != null && !flag)
+//				{
+//					flag = right.add(data, nodeNum, leftNum);
+//				}
+//			}
+//			return flag;
+//		}
+//		
+//		@Override
+//		public String toString() {
+//			return "Node [nodeNum=" + nodeNum + ", data=" + data + "]";
+//		}		
+//	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -59,18 +138,56 @@ public class Solution {
 		for (int t = 1; t <= 10; t++)
 		{
 			N = Integer.parseInt(br.readLine().trim());
-			st = new StringTokenizer(br.readLine().trim());
-			st.nextToken();
-			Node root = new Node(1, st.nextToken());
-			for (int i = 2; i <= N; i++)
+			int flag = 1;
+			for (int i = 1; i <= N; i++)
 			{
 				st = new StringTokenizer(br.readLine().trim());
 				st.nextToken();
-				root.add(st.nextToken(), i);
+				String data = st.nextToken();
+				if (data.length() > 1)
+				{
+					//무조건 숫자여야함
+					if (st.hasMoreTokens())
+					{
+						flag = 0;
+						continue;
+					}
+				}
+				else 
+				{
+					char c = data.charAt(0);
+					if (Character.isDigit(c))
+					{
+						if (st.hasMoreTokens())
+						{
+							flag = 0;
+							continue;
+						}
+					}
+					else
+					{
+						if (st.hasMoreTokens())
+						{
+							st.nextToken();
+							if (!st.hasMoreTokens())
+							{
+								flag = 0;
+								continue;
+							}
+						}
+						else
+						{
+							flag = 0;
+							continue;
+						}
+					}
+				}
 			}
-			int a = 1;
+			System.out.println("#" + t + " " + flag);	
 		}
 		br.close();
+	
+
 	}
 
 }
