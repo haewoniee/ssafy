9aaf2b797826b158c2ef585bd70fecc54bffec1d
한혜원 <hhan9243@gmail.com>
2020-08-06 21:53:55 +0900

    baekjoon 도영이음식

---
+++ src/com/ssafy/baekjoon/p2961_도영이가만든맛있는음식/Solution.java
@@ -0,0 +1,62 @@
+package com.ssafy.baekjoon.p2961_도영이가만든맛있는음식;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int N, R, minDiff;
+	static int[] bitters, sours, chosen;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine().trim());
+		sours = new int[N];
+		bitters = new int[N];
+		minDiff = Integer.MAX_VALUE;
+		StringTokenizer st;
+		for (int i = 0; i < N; i++)
+		{
+			st = new StringTokenizer(br.readLine().trim());
+			sours[i] = Integer.parseInt(st.nextToken());
+			bitters[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (R = 1; R <= N; R++)
+		{
+			chosen = new int[R];
+			nCr(0,0);
+		}
+		bw.write("" + minDiff);
+		bw.newLine();
+		bw.close();
+		br.close();
+	}
+	
+	static void nCr(int cnt, int start)
+	{
+		if (cnt == R)
+		{
+			int bitter = 0, sour = 1;
+			for (int i = 0; i < R; i++)
+			{
+				sour *= sours[chosen[i]];
+				bitter += bitters[chosen[i]];
+			}
+			minDiff = Math.min(minDiff, Math.abs(sour - bitter));
+			return;
+		}
+		
+		for (int i = start; i < N; i++)
+		{
+			chosen[cnt] = i;
+			nCr(cnt+1, i+1);
+		}
+	}
+
+}
