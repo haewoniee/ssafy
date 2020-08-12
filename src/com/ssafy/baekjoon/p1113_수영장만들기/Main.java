03eaa580f16f7d20fb23c5bc78cb27d04a5c7efc
multicampus <multicampus@M805>
2020-08-12 16:41:35 +0900

    swea 미생물격리 완(찝찝), 백준 수영장만들기 미완

--- src/com/ssafy/baekjoon/p1113_수영장만들기/Main.java
+++ src/com/ssafy/baekjoon/p1113_수영장만들기/Main.java
@@ -2,12 +2,15 @@ package com.ssafy.baekjoon.p1113_수영장만들기;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static boolean busted;
-	static int[][] map, visited, filled, dir = {{1,0},{-1,0},{0,1},{0,-1}};
+	static int N, M, cnt;
+	static int[][] map, visited;
+	static int[] dr = { 1, -1, 0, 0 }, dc = { 0, 0, 1, -1 };
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,77 +18,79 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		filled = new int[N][M];
 		visited = new int[N][M];
-		
-		for (int i = 0; i < N; i++)
-		{
+
+		for (int i = 0; i < N; i++) {
 			char[] inp = br.readLine().trim().toCharArray();
-			for (int j = 0; j < M; j++)
-			{
+			for (int j = 0; j < M; j++) {
 				map[i][j] = inp[j] - '0';
 			}
 		}
-		
-		
-		for (int h = 9; h >= 1; h--)
-		{
-			visited = new int[N][M];
 
-			for (int i = 0; i < N; i++)
-			{
-				for (int j = 0; j < M; j++)
-				{
-					if (visited[i][j] == 0 && filled[i][j] == 0)
-					{
-						busted = false;
-						visited[i][j] = 1;
-						flood(i,j,h);						
+		Queue<int[]> que = new LinkedList<int[]>();
+		for (int h = 1; h <= 9; h++) {
+			visited = new int[N][M];
+			visited[0][0] = 1;
+			que.add(new int[] { 0, 0 });
+			while (!que.isEmpty()) {
+				int[] cur = que.poll();
+				boolean busted = false;
+				if (cur[0] == 0 || cur[0] == N - 1 || cur[1] == 0 || cur[1] == M - 1) {
+					busted = true;
+				}
+				int min = Integer.MAX_VALUE;
+				for (int i = 0; i < 4; i++) {
+					int newR = cur[0] + dr[i];
+					int newC = cur[1] + dc[i];
+					
+					if (rangeCheck(newR, newC)) {
+						if (visited[newR][newC] == 0) {
+							visited[newR][newC] = 1;
+							que.add(new int[] { newR, newC });
+							if (map[cur[0]][cur[1]] < h) {
+								if (!busted && (newR == 0 || newR == N - 1 || newC == 0 || newC == M - 1)) {
+									
+									if (map[newR][newC] < h) {
+										busted = true;
+									} else {
+										
+										min = Math.min(min, map[newR][newC]);
+									}
+								} else {
+									
+									min = Math.min(min, map[newR][newC]);
+								}
+							}
+						}
+						
 					}
 				}
-			}
-		}
-		
-		int sum = 0;
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < M; j++)
-			{
-				sum += filled[i][j];
-			}
-		}
-		System.out.println(sum);
-		br.close();
-	}
-	
-	static void flood(int r, int c, int h)
-	{
-		//예외: 경계에 있으면 무조건 물을 채울수 없음
-		if (r == N-1 || r == 0 || c == M-1 || c == 0)
-		{
-			if (map[r][c] < h)
-				busted = true;
-			return;
-		}
-		//DFS로 타고가면서 h보다 낮은 블록의 개수를 리턴
-		if (map[r][c] < h)
-		{
-			filled[r][c] = h - map[r][c];
-			for (int i = 0; i < 4; i++)
-			{
-				int newR = r + dir[i][0];
-				int newC = c + dir[i][1];
-				if (newR >= 0 && newR < N && newC >= 0 && newC < M &&
-						visited[newR][newC] == 0 && map[newR][newC] <= h)
-				{
-					visited[newR][newC] = 1;
-					flood(newR, newC, h);
+				if (!busted && min != Integer.MAX_VALUE && min > map[cur[0]][cur[1]]) {
+					cnt += min - map[cur[0]][cur[1]];
+					map[cur[0]][cur[1]] = min;
+					printMap();
 				}
 			}
-			if (busted)
-			{
-				filled[r][c] = 0;
-			}
 		}
+		System.out.println(cnt);
+		br.close();
 	}
+
+	static void printMap() {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("------------------");
+	}
+
+	static boolean rangeCheck(int r, int c) {
+		if (r >= 0 && r < N && c >= 0 && c < M) {
+			return true;
+		}
+		return false;
+	}
+
 }
