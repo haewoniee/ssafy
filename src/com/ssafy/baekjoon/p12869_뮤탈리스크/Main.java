7cc044e813488d69f08a465eecc3754e4fc57412
haewoniee <hhan9243@gmail.com>
2020-08-18 17:39:44 +0900

    0818 렉쳐 - 채팅서버

---
+++ src/com/ssafy/baekjoon/p12869_뮤탈리스크/Main.java
@@ -0,0 +1,14 @@
+package com.ssafy.baekjoon.p12869_뮤탈리스크;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int[] mutalisk = {9,3,1};
+	static int[][] nPr = {{0,1,2},{0,2,1},{1,0,2},{1,2,0},{2,0,1},{2,1,0}};
+	
+	public static void main(String[] args) {
+		Scanner sc;
+		Arrays.parallelSort(a);
+	}
+}
